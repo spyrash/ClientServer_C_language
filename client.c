0650ca5b781936ef19e2f69139b1aded72f231ed
spyrash <spyrash@debian12.myguest.virtualbox.org>
2024-02-12 23:38:51 +0100

    feat: still a work in progress, managed to use poll in a way for receive and send messages, all in a while true loop, now i need the server

--- client.c
+++ client.c
@@ -1,4 +1,7 @@
 #include <sys/socket.h>
+#include <poll.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 
 int main() {
 	// using the domain AF_INET for IPv4 Internets protocols
@@ -10,18 +13,53 @@ int main() {
 	// binding the socket to a certain address to listen
 	// using the internet domain socket
 	// htons handle the order of bytes between the machine and the socket
-	struct sockaddr_in socketAddress = {
-		AF_INET,
-		htons(1234),
-		0
-	};
+	 struct sockaddr_in socketAddress = {
+        AF_INET,
+        htons(9999),
+        0
+    };
 	
 	bind(socketfd, &socketAddress, sizeof(socketAddress));
 	
 	// mark the socketfd in order to accept incoming connection using accept( by the server)
 	listen(socketfd, 100);
-	// accept the connection 
-	accept(socketfd, 0, 0);
+	// accept the connection, and return the file descriptor of the accepted connection
+	int acceptedfd = accept(socketfd, 0, 0);
 	
+	// create two structure for poll
+    struct pollfd fds[2] = {
+        {
+			0,
+            POLLIN,
+            0
+        },
+        {
+            socketfd,
+            POLLIN,
+            0
+        }
+    };
+    
+    while (true) {
+    char buffer[256] = {0};
+    // it waits for one of a set of file
+    // descriptors to become ready to perform I/O
+    poll(fds, 2, 50000);
+    
+    // POLLIN => There is data to read.
+    if (fds[0].revents & POLLIN) {
+		read(0, buffer, 255);
+		send(acceptedfd, buffer, 255, 0);
+	}
+	
+	if (fds[1].revents & POLLIN) {
+		if (0 == recv(socketfd, buffer, 255, 0)) {
+			return 0;
+			}
+
+            printf("%s\n", buffer);
+		}
+	}
+
 	return 1;
 }
