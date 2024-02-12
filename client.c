71b8a1151f204d728208f177c011ee1a7e32214e
spyrash <spyrash@debian12.myguest.virtualbox.org>
2024-02-12 22:57:24 +0100

    feat: implemented the bind, listen and accept for the connection, still working in progress

--- client.c
+++ client.c
@@ -8,5 +8,20 @@ int main() {
 	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
 	
 	// binding the socket to a certain address to listen
-	bind(
+	// using the internet domain socket
+	// htons handle the order of bytes between the machine and the socket
+	struct sockaddr_in socketAddress = {
+		AF_INET,
+		htons(1234),
+		0
+	};
+	
+	bind(socketfd, &socketAddress, sizeof(socketAddress));
+	
+	// mark the socketfd in order to accept incoming connection using accept( by the server)
+	listen(socketfd, 100);
+	// accept the connection 
+	accept(socketfd, 0, 0);
+	
+	return 1;
 }
